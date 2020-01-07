a094e7789e34ed2f4db1b70c938df354f7cd45a2
m-nick <sunty1@mail.ru>
2020-01-08 02:23:08 +0300

    Clear build.sh

--- build.sh
+++ build.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
 
-# yarn build
-
-# rm -rf ../abz.testing.production/css
-# rm -rf ../abz.testing.production/fonts
-# rm -rf ../abz.testing.production/img
-# rm -rf ../abz.testing.production/js
-# cp -r dist/* ../abz.testing.production/
-# cd ../abz.testing.production/
-# git add --all
-
-# git commit -m "$(date)"
-# git push
-
-
-
-
-
-# остановить публикацию при ошибках
+# stop publicating if errors
 set -e
 
-# сборка
 yarn build
 echo Built!
 
-# переход в каталог сборки
 cd dist
-
-# если вы публикуете на пользовательский домен
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:m-nick/abz.agency.testing.git master:gh-pages
 echo Deployed!
 cd -
