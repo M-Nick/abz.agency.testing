c4dcb4891314efa0986bb06a256ca1bc0378c9d1
m-nick <sunty1@mail.ru>
2020-01-14 21:00:10 +0300

    Fix prod path.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? 'https://m-nick.github.io/abz.agency.testing/'
+      ? 'https://test-abz-agency-345.herokuapp.com/'
       : '/',
 }
